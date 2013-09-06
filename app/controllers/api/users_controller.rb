41c9c7afd1395e47dbcb85548b750613231cb1e8
Theo <bbtfrr@gmail.com>
2013-09-06 12:15:08 +0800

    add sign_in to api/users/login

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -7,15 +7,23 @@ class Api::UsersController < Api::BaseController
   # POST /api/users/login.json
   def login
     @user = BaseUser.find_for_database_authentication(mobile: params[:data][:mobile])
-    # @user.reset_authentication_token!
 
     if @user && @user.valid_password?(params[:data][:password])
-      @user.reset_authentication_token!  # make sure the user has a token generated      
+      @user.reset_authentication_token!  # make sure the user has a token generated
+      sign_in(@user)  
       render_create_success @user, { auth_token: @user.authentication_token }
     else
       warden.custom_failure!
       render_error t('devise.failure.invalid'), :unauthorized
     end
+  end
+
+  # GET /api/users/logout
+  # GET /api/users/logout.json
+  # DELETE /api/users/logout
+  # DELETE /api/users/logout.json
+  def logout
+    sign_out(resource_name)
   end
 
   # POST /api/users
