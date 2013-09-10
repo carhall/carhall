619d8c563368da18a7f60ace2ef232c863210767
Theo <bbtfrr@gmail.com>
2013-09-10 16:05:02 +0800

    add seed for creating posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
+me = User.where(mobile: '13112345678', username: 'user').first_or_initialize
+me.password = 'password' && me.save! if me.new_record?
+
+3.times do 
+  u = FactoryGirl.create :user
+  me.make_friend_with! u
+end
+
+10.times do
+  FactoryGirl.create :post, user: me.friends.sample
+end
+
+30.times do
+  FactoryGirl.create :comment, source: Post.all.sample, user: User.all.sample
+end
