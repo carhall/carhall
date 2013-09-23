8e35b6573bb7a001dd1da23a515aa7f1227de6fc
Theo <bbtfrr@gmail.com>
2013-09-23 10:04:26 +0800

    friendship now use table friend

--- app/models/accounts/friendship.rb
+++ app/models/accounts/friendship.rb
@@ -1,12 +1,15 @@
 class Accounts::Friendship < ActiveRecord::Base
-  # self.table_name = "friend"
+  self.table_name = "friend"
 
   belongs_to :user
   belongs_to :friend, class_name: Account
 
   attr_accessible :friend_id, :user_id
 
+  validates_presence_of :user, :friend
+
   validates_each :friend_id do |record, attr, value|
+    next unless value
     if record.user.blocks.where(blacklist_id: value).any?
       record.errors.add(:base, I18n.t('in_blacklist')) 
     end
