f8f2eabe0bf046fc9fc627d48a8a46593a1eb7ab
Theo <bbtfrr@gmail.com>
2013-10-02 13:20:26 +0800

    update devise failure_app

--- config/initializers/devise_failure_app.rb
+++ config/initializers/devise_failure_app.rb
@@ -2,7 +2,6 @@ module Devise
   FailureApp.class_eval do
 
     def respond
-      p env
       if env["REQUEST_PATH"] =~ /^\/api/
         respond_json
       elsif http_auth?
@@ -14,25 +13,11 @@ module Devise
       end
     end
 
-    def http_auth_body
-      return i18n_message unless request_format
-      method = "to_#{request_format}"
-      if method == "to_xml"
-        { error: i18n_message }.to_xml(root: "errors")
-      elsif method == "to_json"
-        { error: i18n_message, error_code: warden_message, success: false }.to_json
-      elsif {}.respond_to?(method)
-        { error: i18n_message }.send(method)
-      else
-        i18n_message
-      end
-    end
-
     def respond_json
       self.status = 401
       self.headers["WWW-Authenticate"] = %(Basic realm=#{Devise.http_authentication_realm.inspect}) if http_auth_header?
       self.content_type = 'application/json'
-      self.response_body = { error: i18n_message, error_code: warden_message, success: false }.to_json
+      self.response_body = { error: i18n_message, error_code: warden_message || :unauthenticated, success: false }.to_json
     end
   end
 end
