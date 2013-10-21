9e9977674c16bacafe936400539287520e09a5e2
Theo <bbtfrr@gmail.com>
2013-10-21 16:18:11 +0800

    setup logger format

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Carhall::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  config.log_level = :warn
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
@@ -76,5 +76,5 @@ Carhall::Application.configure do
   # config.autoflush_log = false
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
+  # config.log_formatter = ::Logger::Formatter.new
 end
