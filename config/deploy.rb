0780652153937852934f7792e0ac2abfb53dcc9e
Theo <bbtfrr@gmail.com>
2013-10-21 22:56:49 +0800

    rollback api/constants controller

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
-      run "service apache2 restart"
+      execute "service apache2 restart"
     end
   end
 
