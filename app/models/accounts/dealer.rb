949b7279085fc92305bd208093cfaef961aa8e91
Theo <bbtfrr@gmail.com>
2013-10-23 12:27:57 +0800

    fix small bug

--- app/models/accounts/dealer.rb
+++ app/models/accounts/dealer.rb
@@ -113,7 +113,7 @@ class Accounts::Dealer < Accounts::Account
     t.add :mending_goal_attainment, append_to: :detail
     t.add :cleaning_goal_attainment, append_to: :detail
     t.add :bulk_purchasing_goal_attainment, append_to: :detail
-    t.add :mending, template: :base, append_to: :without_dealer
+    t.add :mending, template: :without_dealer, append_to: :detail
     t.add :orders_count, append_to: :detail
     t.add :last_3_orders, append_to: :detail, template: :base
     # t.add :last_3_reviews, append_to: :detail, template: :base
