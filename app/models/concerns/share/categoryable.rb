8c6a08c9b5d5b661234110ce271944605c4b6d70
Theo <bbtfrr@gmail.com>
2013-10-26 09:55:19 +0800

    fix brands cache

--- app/models/concerns/share/categoryable.rb
+++ app/models/concerns/share/categoryable.rb
@@ -8,16 +8,20 @@ module Share::Categoryable
 
     validates_presence_of :name 
     validates_uniqueness_of :name
-    
-    def self.lookup_by_id(index)
-      Rails.cache.fetch([self, index], expires_in: 1.day) do
-        enum_values.find_by_id(index)
-      end
-    end
+
+    extend LookupMethods
   end
   
   def flush_cache
     Rails.cache.delete([self.class, self.id])
+  end
+
+  module LookupMethods
+    def lookup_by_id(index)
+      Rails.cache.fetch([self, index], expires_in: 1.day) do
+        enum_values.find_by_id(index)
+      end
+    end
   end
 
 end
