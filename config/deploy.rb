f7fa93779ecea0442ae530747e895e649af3ebde
Theo <bbtfrr@gmail.com>
2013-10-29 15:47:22 +0800

    fix deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
-      execute "service apache2 restart"
+      execute "service nginx restart"
     end
   end
 
