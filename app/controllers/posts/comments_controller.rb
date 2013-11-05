d375339033945d67e9c9fddede99a16462669a0e
Theo <bbtfrr@gmail.com>
2013-11-06 00:46:03 +0800

    posts/comments with shallow

--- app/controllers/posts/comments_controller.rb
+++ app/controllers/posts/comments_controller.rb
@@ -1,5 +1,5 @@
 class Posts::CommentsController < Posts::ApplicationController
   load_resource :post, class: Posts::Post
-  set_resource_class Posts::Club, through: :post
+  set_resource_class Posts::Club, through: :post, shallow: true
 
 end
