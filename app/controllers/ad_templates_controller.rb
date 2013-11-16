250a1cc011cb922de89f94b48b7018ef486675eb
Theo <bbtfrr@gmail.com>
2013-11-16 22:36:41 +0800

    disable ad_template

--- app/controllers/ad_templates_controller.rb
+++ app/controllers/ad_templates_controller.rb
@@ -1,23 +1,23 @@
-class AdTemplatesController < ApplicationController
-  def index
-    @ad_templates = ::Business::AdTemplate.all
+# class AdTemplatesController < ApplicationController
+#   def index
+#     @ad_templates = ::Business::AdTemplate.all
 
-    @ad_templates = set_filter_id @ad_templates, :product_type
-    @ad_templates = set_filter_id @ad_templates, :product
-  end
+#     @ad_templates = set_filter_id @ad_templates, :product_type
+#     @ad_templates = set_filter_id @ad_templates, :product
+#   end
 
-  def show
-    @ad_template = ::Business::AdTemplate.find(params[:id])
-  end
+#   def show
+#     @ad_template = ::Business::AdTemplate.find(params[:id])
+#   end
 
-  def buy
-    @ad_template = ::Business::AdTemplate.find(params[:id])
-    if @ad_template.buy @current_user
-      send_file @ad_template.file.path
-    else
-      flash[:error] = t("not_enough_balance")
-      redirect_to :back
-    end
-  end
+#   def buy
+#     @ad_template = ::Business::AdTemplate.find(params[:id])
+#     if @ad_template.buy @current_user
+#       send_file @ad_template.file.path
+#     else
+#       flash[:error] = t("not_enough_balance")
+#       redirect_to :back
+#     end
+#   end
   
-end
+# end
