b2bf1fd52bf3a3abbef0c2d32739ddc45147a193
Theo <bbtfrr@gmail.com>
2013-11-17 23:57:16 +0800

    change avatar medium style

--- app/models/concerns/share/image_attachments.rb
+++ app/models/concerns/share/image_attachments.rb
@@ -1,6 +1,6 @@
 module Share::ImageAttachments
   def define_avatar_method options={}
-    has_attached_file :avatar, { styles: { medium: "300x300#", thumb: "60x60#" }}.merge(options)
+    has_attached_file :avatar, { styles: { medium: "300x200>", thumb: "60x60#" }}.merge(options)
   end
 
   def define_image_method options={}
