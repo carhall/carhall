b4c5366cad4f9a1e47d4cf9c067e03e27d0bfcf4
Theo <bbtfrr@gmail.com>
2013-11-21 10:19:41 +0800

    enable ad_template & tutorial

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,13 +1,13 @@
-# class TutorialsController < ApplicationController
-#   def index
-#     @tutorials = ::Business::Tutorial.all
+class TutorialsController < ApplicationController
+  def index
+    @tutorials = ::Business::Tutorial.all
 
-#     @tutorials = set_filter_id @tutorials, :product_type
-#     @tutorials = set_filter_id @tutorials, :product
-#   end
+    @tutorials = set_filter_id @tutorials, :product_type
+    @tutorials = set_filter_id @tutorials, :product
+  end
 
-#   def show
-#     @tutorial = ::Business::Tutorial.find(params[:id])
-#   end
+  def show
+    @tutorial = ::Business::Tutorial.find(params[:id])
+  end
   
-# end
+end
