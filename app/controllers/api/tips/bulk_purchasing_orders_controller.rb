153184536c9fcbca79ac255396b8fddce65d7425
Theo <bbtfrr@gmail.com>
2013-12-07 20:24:14 +0800

    fix vip_card_orders issue

--- app/controllers/api/tips/bulk_purchasing_orders_controller.rb
+++ app/controllers/api/tips/bulk_purchasing_orders_controller.rb
@@ -1,15 +1,7 @@
 class Api::Tips::BulkPurchasingOrdersController < Api::Tips::OrdersController
-  before_filter :set_current_user
-  set_resource_class ::Tips::BulkPurchasingOrder, detail: true
-  before_filter :set_order, only: [:finish, :use, :cancel, :review]
-  before_filter :set_filter
 
   def set_parent
     @parent = @current_user.bulk_purchasing_orders
-  end
-
-  def set_filter
-    filter_parent :state
   end
   
 end
--- app/controllers/api/tips/cleaning_orders_controller.rb
+++ app/controllers/api/tips/cleaning_orders_controller.rb
@@ -1,15 +1,7 @@
 class Api::Tips::CleaningOrdersController < Api::Tips::OrdersController
-  before_filter :set_current_user
-  set_resource_class ::Tips::CleaningOrder, detail: true
-  before_filter :set_order, only: [:finish, :use, :cancel, :review]
-  before_filter :set_filter
 
   def set_parent
     @parent = @current_user.cleaning_orders
-  end
-
-  def set_filter
-    filter_parent :state
   end
   
 end
