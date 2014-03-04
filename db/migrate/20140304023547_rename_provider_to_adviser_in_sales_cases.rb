47d043999853c3aa9c5dcf1875310a21eb62e606
Theo <bbtfrr@gmail.com>
2014-03-04 14:02:03 +0800

    add state_id to sales_case

--- db/migrate/20140304023547_rename_provider_to_adviser_in_sales_cases.rb
+++ db/migrate/20140304023547_rename_provider_to_adviser_in_sales_cases.rb
@@ -1,7 +1,10 @@
 class RenameProviderToAdviserInSalesCases < ActiveRecord::Migration
   def change
     change_table :sales_cases do |t|
-      t.rename :provider, :adviser
+      t.rename  :provider, :adviser
+      
+      t.integer :state_id
+      t.index   :state_id
       
       t.timestamps
     end
