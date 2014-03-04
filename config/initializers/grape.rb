a84a8910635ba3502fb3fcf54acaa762c3efc254
root <root@AY130816083627504b44Z.(none)>
2014-03-04 12:46:10 +0800

    fix openfire upload_file issue

--- config/initializers/grape.rb
+++ config/initializers/grape.rb
@@ -2,6 +2,21 @@ def absolute_image_url image, type
   "#{AbsoluteUrlPrefix}#{image.url(type, timestamp: false)}"
 end
 
+module Paperclip
+  class HashieMashUploadedFileAdapter < AbstractAdapter
+
+    def initialize(target)
+      @tempfile, @content_type, @size = target.tempfile, target.type, target.tempfile.size
+      self.original_filename = target.filename
+    end
+
+  end
+end
+
+Paperclip.io_adapters.register Paperclip::HashieMashUploadedFileAdapter do |target|
+  target.is_a? Hashie::Mash
+end
+
 Grape::Entity.class_eval do
   format_with :image do |image|
     if image.present?
