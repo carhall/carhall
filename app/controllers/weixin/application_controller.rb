28457a8b31c06a6d096a13d9ca9576e804509c69
Theo <bbtfrr@gmail.com>
2014-05-06 20:09:21 +0800

    turn down weixin oauth

--- app/controllers/weixin/application_controller.rb
+++ app/controllers/weixin/application_controller.rb
@@ -27,15 +27,4 @@ class Weixin::ApplicationController < ApplicationController
   end
 
   helper_method :weixin_polymorphic_path
-
-  def authenticate_weixin_account!
-    unless current_weixin_account
-      if params[:dealer_id]
-        dealer = Accounts::Dealer.find(params[:dealer_id])
-        redirect_to "https://open.weixin.qq.com/connect/oauth2/authorize?appid=#{dealer.weixin_app_id}&redirect_uri=#{CGI::escape(weixin_accounts_check_url)}&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect"
-      else
-        redirect_to new_weixin_account_session_path
-      end
-    end
-  end
 end
