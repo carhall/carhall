b55923b8689bf978437fe6a48b3ba7bca9d8b1e3
Theo <bbtfrr@gmail.com>
2014-05-06 15:53:37 +0800

    update weixin website orders

--- app/controllers/weixin/application_controller.rb
+++ app/controllers/weixin/application_controller.rb
@@ -7,4 +7,24 @@ class Weixin::ApplicationController < ApplicationController
     super klass, options.reverse_merge(no_authorize: true, through: :dealer)
   end
 
+  def weixin_polymorphic_path *args
+    options = args.extract_options!
+    paths = []
+    paths << options[:action] if options[:action]
+    paths << :weixin
+    records = []
+    args.each do |arg|
+      paths << case arg
+      when ActiveRecord::Base
+        records << arg
+        arg.class.model_name.element
+      else
+        arg.to_s        
+      end
+    end
+    paths << :path
+    send paths.join("_"), *records, options
+  end
+
+  helper_method :weixin_polymorphic_path
 end
