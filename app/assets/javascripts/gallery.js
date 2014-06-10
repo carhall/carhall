afbbf6ca68ee2cbd03397d7546ab22aaad00070f
Theo <bbtfrr@gmail.com>
2014-06-10 10:52:34 +0800

    weixin website: remove hermitage navigationButtons & closeButton

--- app/assets/javascripts/gallery.js
+++ app/assets/javascripts/gallery.js
@@ -1,2 +1,8 @@
 //= require jquery
 //= require hermitage
+
+// hermitage.navigationButtons.enabled = false
+hermitage.navigationButtons.next.text = '>'
+hermitage.navigationButtons.previous.text = '<'
+
+hermitage.closeButton.enabled = false
